1adbaf9bf1b53d0e693f10e655351deb032ba106
Dylan Mooers <dmooers@calpoly.edu>
2021-08-26 20:19:16 -0700

    Adding in hooks

--- src/state/stablePools/hooks.ts
+++ src/state/stablePools/hooks.ts
@@ -1,1 +1,13 @@
 // To-Do: Implement Hooks to update Client-Side contract representation
+import { ChainId } from '@ubeswap/sdk'
+
+import { STATIC_POOL_INFO } from '../../constants/StablePools'
+
+export const initPools = (chainId: ChainId) => {
+  //const dispatch = useDispatch()
+  const poolsForChainId = STATIC_POOL_INFO[chainId]
+
+  poolsForChainId.forEach((pool) => {
+    console.log(pool)
+  })
+}
