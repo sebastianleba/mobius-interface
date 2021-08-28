ff7181273d5a3e26d1c524a9fe5610d6e58875c6
Dylan Mooers <dmooers@calpoly.edu>
2021-08-28 07:45:16 -0700

    hooks to get more info from pools

--- src/state/stablePools/hooks.ts
+++ src/state/stablePools/hooks.ts
@@ -1,6 +1,7 @@
 // To-Do: Implement Hooks to update Client-Side contract representation
 import { useSelector } from 'react-redux'
 
+import { StableSwapMath } from '../../utils/stableSwapMath'
 import { AppState } from '..'
 import { StableSwapPool } from './reducer'
 
@@ -13,10 +14,23 @@ export function useCurrentPool(tok1: string, tok2: string): readonly [StableSwap
   return [pools.length > 0 ? pools[0] : null]
 }
 
+export function usePools(): readonly StableSwapPool[] {
+  const pools = useSelector<AppState, StableSwapPool[]>((state) =>
+    Object.values(state.stablePools.pools).map(({ pool }) => pool)
+  )
+  return pools
+}
+
+export function useMathUtil(pool: StableSwapPool | string): readonly [StableSwapMath] {
+  const name = typeof pool == 'string' ? pool : pool.name
+  const math = useSelector<AppState, StableSwapMath>((state) => state.stablePools.pools[name].math)
+  return [math]
+}
+
 export function usePool(): readonly [StableSwapPool] {
   const [tok1, tok2] = useSelector<AppState, [string, string]>((state) => [
     state.swap.INPUT.currencyId,
     state.swap.OUTPUT.currencyId,
   ])
-  return getCurrentPool(tok1, tok2)
+  return useCurrentPool(tok1, tok2)
 }
