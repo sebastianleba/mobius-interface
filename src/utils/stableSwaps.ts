7a7da1d347174ce91b30107e59ff234e79e1cce2
Dylan Mooers <dmooers@calpoly.edu>
2021-09-19 17:38:26 -0700

    Fixing btc display

--- src/utils/stableSwaps.ts
+++ src/utils/stableSwaps.ts
@@ -1,15 +1,47 @@
-import { Token } from '@ubeswap/sdk'
+import { JSBI, Token, TokenAmount } from '@ubeswap/sdk'
 
-export type SwappableToken = Token & {
-  swapAddress: string
+import { StablePoolInfo } from '../state/stablePools/hooks'
+
+const dummyToken = new Token(1, '0x8cD0E2F11ed2E896a8307280dEEEE15B27e46BbE', 18, 'MobLP', 'Mobius cBTC/wBTC LP')
+
+const dummyAmount = new TokenAmount(dummyToken, '0')
+
+const scaleAmount =
+  (toScale: JSBI) =>
+  (ta: TokenAmount | undefined, addition = JSBI.BigInt('0')) =>
+    toScale && ta
+      ? new TokenAmount(ta.token, JSBI.multiply(JSBI.add(addition, ta.raw), toScale))
+      : new TokenAmount(dummyToken, '0')
+
+export const getDepositValues = (
+  pool: StablePoolInfo | undefined
+): {
+  valueOfStaked: TokenAmount
+  valueOfDeposited: TokenAmount
+  totalValueDeposited: TokenAmount
+  totalValueStaked: TokenAmount
+} => {
+  if (!pool)
+    return {
+      valueOfDeposited: dummyAmount,
+      valueOfStaked: dummyAmount,
+      totalValueDeposited: dummyAmount,
+      totalValueStaked: dummyAmount,
+    }
+  const { totalDeposited, amountDeposited, virtualPrice, stakedAmount, totalStakedAmount, lpToken } = pool
+  const scale = scaleAmount(virtualPrice)
+
+  const valueOfStaked = scale(stakedAmount)
+  const valueOfDeposited = scale(amountDeposited, stakedAmount?.raw)
+  const totalValueStaked = scale(totalStakedAmount)
+  const totalValueDeposited = scale(totalDeposited, totalStakedAmount?.raw)
+  return {
+    valueOfStaked,
+    valueOfDeposited,
+    totalValueDeposited,
+    totalValueStaked,
+  }
 }
-
-export type StablePool = {
-  token0: Token
-  token1: Token
-  token2: Token
-}
-
 // export const getSwappableTokens(tokenIn: Token):  SwappableToken[]{
 //     const defaultTokens = useDefaultTokens();
 // }
